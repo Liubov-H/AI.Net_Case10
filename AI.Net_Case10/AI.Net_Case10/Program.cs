61340b6af57649da0bc4409a04fff3129e73fce7
Liubov Havlo <luba22www@gmail.com>
2023-06-27 02:37:36 +0300

    UC#10 Implement regex

--- AI.Net_Case10/AI.Net_Case10/Program.cs
+++ AI.Net_Case10/AI.Net_Case10/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+	static void Main()
+	{
+		string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!""#$%&'()*+,-./:;<=>?@\[\]^_`{|}~])([^\s]{40,50})$";
+		Regex regex = new Regex(pattern);
+
+		while (true)
+		{
+			Console.Write("Enter a string (or 'quit' to stop): ");
+			string userInput = Console.ReadLine();
+
+			if (userInput.ToLower() == "quit")
+			{
+				break;
+			}
+
+			if (regex.IsMatch(userInput))
+			{
+				Console.WriteLine("Match");
+			}
+			else
+			{
+				Console.WriteLine("No match");
+			}
+		}
+	}
+}
